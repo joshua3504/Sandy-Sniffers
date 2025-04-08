2aa04d25b7b383027de1c294fc0322ee51680286
joshua3504 <terrorusii@gmail.com>
2025-04-08 17:16:33 -0400

    Made sniffers able to sniff on sand and added the updated loot table and made them sniff up sand when they sniff on sand.

--- src/main/java/net/joshua3504/sandysniffers/SandySniffersDataGenerator.java
+++ src/main/java/net/joshua3504/sandysniffers/SandySniffersDataGenerator.java
@@ -2,10 +2,13 @@ package net.joshua3504.sandysniffers;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.joshua3504.sandysniffers.datagen.ModBlockTagProvider;
 
 public class SandySniffersDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
 	}
 }
---
+++ src/main/java/net/joshua3504/sandysniffers/mixin/SnifferSandMixin.java
@@ -0,0 +1,48 @@
+package net.joshua3504.sandysniffers.mixin;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.passive.SnifferEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SnifferEntity.class)
+public abstract class SnifferSandMixin {
+    @Accessor("FINISH_DIG_TIME")
+    protected static final TrackedData<Integer> getFinishDigTime() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Invoker("getDigPos")
+    protected abstract BlockPos invokeGetDigPos();
+
+    @Inject(method = "dropSeeds", at = @At("HEAD"))
+    protected void addSand(CallbackInfo info) {
+        SnifferEntity sniffer = (SnifferEntity) (Object) this;
+
+        if (!(sniffer.getWorld() instanceof ServerWorld serverWorld)) {
+            return;
+        }
+
+        if (!sniffer.getWorld().isClient() && sniffer.getDataTracker().get(getFinishDigTime()) == sniffer.age) {
+            BlockPos digPos = invokeGetDigPos();
+            digPos = new BlockPos(digPos.getX(), digPos.getY() - 1, digPos.getZ());
+
+            if (serverWorld.getBlockState(digPos).getBlock().equals(Blocks.SAND)) {
+                ItemEntity itemEntity = new ItemEntity(serverWorld, digPos.getX(), digPos.getY() + 1, digPos.getZ(),
+                        new ItemStack(Items.SAND, (int) (Math.random() * 4) + 1));
+                itemEntity.setToDefaultPickupDelay();
+                serverWorld.spawnEntity(itemEntity);
+            }
+        }
+    }
+}
